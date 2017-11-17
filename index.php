88733a48d891e6c6f2ee8830bc6dcbb958d614ed
crossjas_mac <crossjae@163.com>
2017-11-17 17:32:47 +0800

    add tag

--- index.php
+++ index.php
@@ -13,10 +13,11 @@
     $episode = $_POST['episode']; // 获取表格的集数
     $name = $_POST['name']; // 获取表格的名字
     $des = $_POST['des']; // 获取表格的描述
+    $tag = $_POST['tag']; // 获取表格的描述
 
     // insert
-    $insert_query = "insert into " . DB_TABLE . " (episode, name, des)" .
-      "values ('" . $episode . "','" . $name . "','" . $des . "')";
+    $insert_query = "insert into " . DB_TABLE . " (episode, name, des, tag_id)" .
+      "values ('" . $episode . "','" . $name . "','" . $des . "','" . $tag . "')";
 
     $insert_result = mysqli_query($dbc, $insert_query)
       or die('Error insert_result');
--- view.php
+++ view.php
@@ -10,16 +10,62 @@
 
   $data = mysqli_query($dbc, $query)
     or die('Error data');
-
+  $tag_query = "select * from " . DB_TAG;
+?>
+<table>
+  <tbody>
+    <tr>
+      <td>集数</td>
+      <td>名字</td>
+      <td>描述</td>
+      <td>
+        标签
+        <select id="s" onchange="changetest()">
+          <?php
+            $tag_data1 = mysqli_query($dbc, $tag_query)
+              or die('error tagdata');
+            while($tag_row1 = mysqli_fetch_array($tag_data1)){
+              echo '<option value="">' . $tag_row1['name'] . '</option>';
+            }
+          ?>
+        </select>
+      </td>
+      <td>操作</td>
+    </tr>
+<?php
   // 输出表格
-  echo '<table><tbody><tr><td>集数</td><td>名字</td><td>描述</td><td>操作</td></tr>';
+  // echo '<tr><td>集数</td><td>名字</td><td>描述</td><td>标签</td><td>操作</td></tr>';
   while($row = mysqli_fetch_array($data)){
-    echo '<tr>' .
+
+    echo '<tr data-id=' . $row['episode'] . '>' .
       '<td>' . $row['episode'] . '</td>' .
       '<td>' . $row['name'] . '</td>' .
-      '<td>' . $row['des'] . '</td>' .
-      '<td><a href="del.php?id=' . $row['episode'] . '">del</a></td>' .
+      '<td>' . $row['des'] . '</td>';
+
+    // 为什么只能在循环里创建新查询？
+
+    $tag_data = mysqli_query($dbc, $tag_query)
+        or die('error tagdata');
+
+    // add tag start
+    // 这部分循环跟js的思路有差异..
+    $hastag = 0; // set a flag
+    while($tag_row = mysqli_fetch_array($tag_data)){
+      if($row['tag_id'] == $tag_row['tag_id']){
+        echo '<td>' . $tag_row['name'] . '</td>';
+        $hastag = 1;
+      }
+    }
+    if($hastag === 0){
+      echo '<td>无</td>';
+    }
+    // add tag end
+
+    echo '<td><a href="del.php?id=' . $row['episode'] . '">del</a></td>' .
       '</tr>';
   }
-  echo '</tbody></table>';
+  // echo '</tbody></table>';
 ?>
+
+  </tbody>
+</table>
